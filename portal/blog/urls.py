6733e09afa728ff6813d707c2c7afd06bebcde56
caio <caiosandor16@gmail.com>
2026-09-23 20:05:09 -0300

    atualizando htlms para a nova versao

--- portal/blog/urls.py
+++ portal/blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('posts/', views.lista_posts, name='lista'),
     path('posts/novo/', views.criar_post, name='criar_post'),
     path('posts/<int:id>/', views.post_detalhe, name='detalhe'),
-    path('rascunhos/', views.lista_rascunhos, name='rascunhos'), # Nova rota do formulário
+    path('rascunhos/', views.lista_rascunhos, name='rascunhos'),
+    path('posts/<int:id>/editar/', views.editar_post, name='editar'),
+    path('posts/<int:id>/excluir/', views.excluir_post, name='excluir'),
 ]
