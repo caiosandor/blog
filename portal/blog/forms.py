f2bd80d0ad4f45786d4a267f99fbd8059fbe735c
caio <caiosandor16@gmail.com>
2026-09-23 19:47:09 -0300

    foi adicionado o metodo clean(self) que agora verifica o status do e o tamanho do conteudo a publicação. se o status for "publicado" e o tamanho do arquivo for menor que 50 caracteres ele gera um erro. antes no casa nao acontecia nada, so verificava se o campo estava vazio ou nao

--- portal/blog/forms.py
+++ portal/blog/forms.py
@@ -1,9 +1,20 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Post
 
-
 class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["titulo", "conteudo", "autor", "status"]
 
-  class Meta:
-    model = Post
-    fields = ["titulo", "conteudo", "autor", "status"]
+    def clean(self):
+        cleaned_data = super().clean()
+        conteudo = cleaned_data.get("conteudo")
+        status = cleaned_data.get("status")
+
+        # Regra da P1: Validar tamanho mínimo se o status for "publicado"
+        if status == 'publicado' and conteudo:
+            if len(conteudo.strip()) < 50:
+                raise ValidationError("Para ser publicado, o conteúdo da notícia deve ter no mínimo 50 caracteres.")
+                
+        return cleaned_data
